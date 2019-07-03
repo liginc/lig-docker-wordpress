c1e1bb68223c5d5088164e4a808c89ef5ca7f143
moriyama_ryota+codecommit@liginc.co.jp-at-349121543715 <moriyama_ryota@liginc.co.jp>
2019-07-04 02:14:21 +0900

    Modify for lig-interactive-generator

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -4,19 +4,12 @@ echo >&2 "Waiting MySQL to be up and running..."
 wait-for-it.sh -t 0 mysql:3306
 echo >&2 "MySQL database is now ready to handle connection."
 
-# Delete wp-config
-if [ -f ${WP_ROOT}/wp-config.php ]; then
-	echo >&2 "wp-config.php was detected. Deleting..."
-	rm -f ${WP_ROOT}/wp-config.php
-fi
-
-# Then initialize a local wp-config if not exists
-if [ -f ${WP_ROOT}/wp-config.local.php ]; then
-	echo >&2 "wp-config.local.php was detected. Skipping downloading Wordpress files..."
-else
+# Download and Install WordPress
+if [ ! -f ${WP_ROOT}/wp-config.php ]; then
 	echo >&2 "Downloading Wordpress files..."
 	wp core download --path=${WP_ROOT} --allow-root --version=${WP_VERSION}
-	echo >&2 "Setting up wp-config.php..."
+
+	echo >&2 "Setting up config file..."
 	wp core config --path=${WP_ROOT} --allow-root \
 		--dbname=${MYSQL_DATABASE} \
 		--dbuser=root \
@@ -32,68 +25,84 @@ define( 'WP_DEBUG', true );
 define( 'WP_DEBUG_DISPLAY', true );
 define( 'WP_DEBUG_LOG', true );
 PHP
-	mv ${WP_ROOT}/wp-config.php ${WP_ROOT}/wp-config.local.php
-fi
 
-# Make a symlink to wp-config
-(cd ${WP_ROOT} && ln -s wp-config.local.php wp-config.php)
+    echo >&2 "Removing wp-config-sapmle.ph...p"
+	rm -f ${WP_ROOT}/wp-config-sample.php
 
-if $(wp core is-installed --path=${WP_ROOT} --allow-root); then
-	echo >&2 "Wordpress seems to be installed."
-else
-	echo >&2 "Installing Wordpress..."
-	# Install core
-	wp core install --path=${WP_ROOT} --allow-root \
-		--url=${WP_URL} \
-		--title=wp \
-		--admin_user=${WP_ADMIN_USER} \
-		--admin_password=${WP_ADMIN_PASSWORD} \
-		--admin_email=${WP_ADMIN_EMAIL} \
-		--skip-email
-    echo "Initialize WordPress"
-	wp language core install ja --allow-root
-	wp option update timezone_string 'Asia/Tokyo' --allow-root
-	wp option update WPLANG 'ja' --allow-root
-	wp option update blog_public '0' --allow-root
-	wp option update default_ping_status 'closed' --allow-root
-	wp option update default_comment_status 'closed' --allow-root
-	wp comment delete 1 --force --allow-root
-	wp post delete 1 2 3 4 --force --allow-root
+    # Init Project
+    if [ ! -e ${SQL_DUMP_DATA} ]; then
 
-    # Remove default installed plugins
+        echo "There is no sql dump file"
+
+        echo "Installing WordPress..."
+        # Install core
+        wp core install --path=${WP_ROOT} --allow-root \
+            --url=${WP_URL} \
+            --title=wp \
+            --admin_user=${WP_ADMIN_USER} \
+            --admin_password=${WP_ADMIN_PASSWORD} \
+            --admin_email=${WP_ADMIN_EMAIL} \
+            --skip-email
+
+        echo >&2 "Installing Japanese language file..."
+        wp language core install ja --allow-root
+
+        echo "Setting up options..."
+        wp option update timezone_string 'Asia/Tokyo' --allow-root
+        wp option update WPLANG 'ja' --allow-root
+        wp option update blog_public '0' --allow-root
+        wp option update default_ping_status 'closed' --allow-root
+        wp option update default_comment_status 'closed' --allow-root
+        wp comment delete 1 --force --allow-root
+        wp post delete 1 2 3 4 --force --allow-root
+
+        # Install extra plugins specified by $WP_INSTALL_PLUGINS
+        if [[ -z "${WP_INSTALL_PLUGINS}" ]]; then
+            echo >&2 "env var \$WP_INSTALL_PLUGINS is empty - skipping installing extra plugins";
+        else
+            echo >&2 "Downloading extra plugins"
+            for TEMP_WP_PLUGIN in $WP_INSTALL_PLUGINS; do
+                echo >&2 "Installing extra plugin ${TEMP_WP_PLUGIN}..."
+                if ! $(wp plugin is-installed ${TEMP_WP_PLUGIN} --path=${WP_ROOT} --allow-root); then
+                    wp plugin install ${TEMP_WP_PLUGIN} --path=${WP_ROOT} --allow-root
+                    echo >&2 "Activating plugins..."
+                    wp plugin activate ${TEMP_WP_PLUGIN} --path=${WP_ROOT} --allow-root
+                fi
+            done
+            unset "TEMP_WP_PLUGIN"
+        fi
+    else
+        echo >&2 "The Project seems to be already started."
+
+        echo >&2 "Installing Japanese language file..."
+        wp language core install ja --allow-root
+
+        # Install core
+        echo >&2 "Installink WordPress..."
+        wp core install --path=${WP_ROOT} --allow-root \
+            --skip-email
+    fi
+
+    # Delete default plugins
     wp plugin delete akismet --allow-root
     wp plugin delete hello --allow-root
 
-    # Install extra plugins specified by $WP_INSTALL_PLUGINS
-    if [[ -z "${WP_INSTALL_PLUGINS}" ]]; then
-        echo >&2 "env var \$WP_INSTALL_PLUGINS is empty - skipping installing extra plugins";
-    else
-        for TEMP_WP_PLUGIN in $WP_INSTALL_PLUGINS; do
-            echo >&2 "Installing extra plugin ${TEMP_WP_PLUGIN}..."
-            if ! $(wp plugin is-installed ${TEMP_WP_PLUGIN} --path=${WP_ROOT} --allow-root); then
-                wp plugin install ${TEMP_WP_PLUGIN} --path=${WP_ROOT} --allow-root
-            fi
-        done
-        unset "TEMP_WP_PLUGIN"
-    fi
-    wp plugin activate --all --allow-root
+    # Delete Default theme
+    rm -Rf ${WP_ROOT}/wp-content/themes/twenty*
+else
+	echo >&2 "Wordpress seems to be installed."
 fi
 
 # Activate theme
 if $(wp theme is-installed twentynineteen --allow-root); then
     wp theme activate ${WP_THEME_NAME} --path=${WP_ROOT} --allow-root
 
-    # Delete theme
-    wp theme delete $(wp theme list --status=inactive --field=name --allow-root) --allow-root
-
-    if ! $(wp core is-installed --path=${WP_ROOT} --allow-root); then
-        echo >&2 "WARNING: It seems that wrong params was set to docker.env - press Ctrl+C now if this is an error!"
-    fi
 fi
 
 envs=(
 	MYSQL_ROOT_PASSWORD
 	MYSQL_DATABASE
+	SQL_DUMP_DATA
 	WP_URL
 	WP_ROOT
 	WP_VERSION
@@ -109,7 +118,5 @@ envs=(
 for e in "${envs[@]}"; do
 	unset "$e"
 done
-
-echo >&2 "Wordpress initialization completed!"
 
 exec "$@"
